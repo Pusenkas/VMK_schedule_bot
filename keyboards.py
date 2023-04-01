64e3c625153a1a0c95d8189bb0b239e9355090e2
SerGoMachine <sereganovik2002@gmail.com>
2023-04-01 13:20:19 +0300

    Реализована пользовательская клавиатура
    В данном python-файле находится реализация пользовательской клавиатуры.
    Созданы кнопки help и schedule

---
+++ keyboards.py
@@ -0,0 +1,11 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.callback_data import CallbackData
+
+
+def get_start_kb() -> ReplyKeyboardMarkup:
+	kb = ReplyKeyboardMarkup(keyboard=[
+		[KeyboardButton('/help'), 
+		 KeyboardButton('/schedule')]
+		], resize_keyboard=True)
+
+	return kb

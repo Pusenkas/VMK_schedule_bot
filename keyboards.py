4aed27770e30a3ce8005cb4db21cbbdba3e3532a
SerGoMachine <sereganovik2002@gmail.com>
2023-04-02 16:34:37 +0300

    Пользовательская клавиатура
    Добавлены две пользовательские клавиатуры, соответствующие состояниям:
    - ожидание опции (кнопки - /today_schedule, /week_schedule)
    - заключительное состояние (кнопки - /return_to_menu, /change_group_number)

--- keyboards.py
+++ keyboards.py
@@ -8,3 +8,19 @@ def get_start_kb() -> ReplyKeyboardMarkup:
          KeyboardButton('/schedule')]
     ], resize_keyboard=True)
     return kb
+
+
+def get_schedule_option_kb() -> ReplyKeyboardMarkup:
+	kb = ReplyKeyboardMarkup(keyboard=[
+		[KeyboardButton('/today_schedule'),
+		 KeyboardButton('/week_schedule')]
+	], resize_keyboard=True)
+	return kb
+
+
+def get_cancel_kb() -> ReplyKeyboardMarkup:
+	kb = ReplyKeyboardMarkup(keyboard=[
+		[KeyboardButton('/return_to_menu'),
+		 KeyboardButton('/change_group_number')]
+	], resize_keyboard=True)
+	return kb

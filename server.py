2b537af49b0c0328d4f1dfe60ac14ce01bbcb84f
SerGoMachine <sereganovik2002@gmail.com>
2023-04-02 16:32:37 +0300

    Телеграм-бот
    Добавлены состояния:
    - ожидание опции (расписание сегодня / расписание на неделю)
    - расписание сегодня
    - расписание на неделю
    - заключительное состояние

--- server.py
+++ server.py
@@ -4,27 +4,30 @@ from keyboards import *
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from pdf_parser import Parser
+
 
 bot = Bot(TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-class StudentStateGroup(StatesGroup):
+class StudentGroupState(StatesGroup):
     processing = State()
     group_number = State()
+    option_proccesing = State()
+    today_schedule = State()
+    week_schedule = State()
+    dump = State()
 
 
-async def cmd_schedule(message: types.Message) -> None:
-    await bot.send_message(chat_id=message.from_user.id,
-                           text='Введите номер вашей учебной группы')
-
-    await StudentStateGroup.group_number.set()
-
-
-async def on_startup(_):
+async def on_startup(_) -> None:
     print('Bot has been started')
+
+
+async def cmd_schedule(message: types.Message, reply_markup: ReplyKeyboardMarkup) -> None:
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Введите номер вашей учебной группы',
+                           reply_markup=reply_markup)
 
 
 @dp.message_handler(commands=['start'], state='*')
@@ -33,8 +36,7 @@ async def cmd_start(message: types.Message) -> None:
                            text='Добро пожаловать!\n'
                                 'Я бот, отправляющий расписание ВМК',
                            reply_markup=get_start_kb())
-
-    await StudentStateGroup.processing.set()
+    await StudentGroupState.processing.set()
 
 
 @dp.message_handler(commands=['help'], state='*')
@@ -45,33 +47,77 @@ async def cmd_help(message: types.Message) -> None:
                                 '/schedule - получить расписание')
 
 
-@dp.message_handler(commands=['schedule'], state=StudentStateGroup.processing)
-async def schedule_wrapper(message: types.Message) -> None:
-    await cmd_schedule(message)
-    await StudentStateGroup.group_number.set()
+@dp.message_handler(commands=['schedule'], state=StudentGroupState.processing)
+async def handle_schedule_while_processing(message: types.Message) -> None:
+    await cmd_schedule(message, reply_markup=get_start_kb())
+    await StudentGroupState.group_number.set()
 
 
-@dp.message_handler(commands=['schedule'], state=StudentStateGroup.group_number)
-async def handle_schedule_while_state(message: types.Message, state: FSMContext) -> None:
-    await cmd_schedule(message)
+@dp.message_handler(commands=['schedule'], state=[StudentGroupState.group_number, StudentGroupState.option_proccesing])
+async def handle_schedule_while_number(message: types.Message) -> None:
+    await cmd_schedule(message, reply_markup=get_start_kb())
+    await StudentGroupState.group_number.set()
 
 
-@dp.message_handler(lambda message: message.text.isdigit(), state=StudentStateGroup.group_number)
+@dp.message_handler(commands=['today_schedule'], state=StudentGroupState.option_proccesing)
+async def cmd_today_schedule(message: types.Message) -> None:
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Держите ваше расписание на сегодня',
+                           reply_markup=get_cancel_kb())
+    await StudentGroupState.dump.set()
+
+
+@dp.message_handler(commands=['return_to_menu'], state=StudentGroupState.dump)
+async def cmd_cancel(message: types.Message) -> None:
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Вы вернулись в главное меню',
+                           reply_markup=get_start_kb())
+    await StudentGroupState.processing.set()
+
+
+@dp.message_handler(commands=['change_group_number'], state=StudentGroupState.dump)
+async def cmd_change(message: types.Message, state: FSMContext) -> None:
+    await cmd_schedule(message, reply_markup=get_start_kb())
+    await StudentGroupState.group_number.set()
+
+
+@dp.message_handler(lambda message: message.text.isdigit(), state=StudentGroupState.group_number)
 async def handle_number(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['number'] = message.text
-    await bot.send_message(chat_id=message.from_user.id, text='Держите ваше расписание!')
-    await bot.send_message(chat_id=message.from_user.id, text=Parser.get_lesson_from_json("data.json", message.text))
-    await StudentStateGroup.processing.set()
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Выберите опцию',
+                           reply_markup=get_schedule_option_kb())
+    await StudentGroupState.option_proccesing.set()
 
 
-@dp.message_handler(lambda message: not message.text.isdigit(), state=StudentStateGroup.group_number)
+@dp.message_handler(lambda message: not message.text.isdigit(), state=StudentGroupState.group_number)
 async def handle_wrong_number(message: types.Message, state: FSMContext) -> None:
-    await message.reply('Неверный номер группы!\n'
-                        'Отправьте номер заново')
+    await message.reply(text='Неверный номер группы!\n'
+                             'Повторите попытку',
+                        reply_markup=get_start_kb())
+
+
+@dp.message_handler(state=StudentGroupState.dump)
+async def handle_message_while_dump(message: types.Message, state: FSMContext) -> None:
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Выберите опцию!',
+                           reply_markup=get_cancel_kb())
+
+
+@dp.message_handler(state=StudentGroupState.option_proccesing)
+async def handle_message_while_opt(message: types.Message, state: FSMContext) -> None:
+    await message.reply(text='Выберите одну из предложенных опций',
+                        reply_markup=get_schedule_option_kb())
+
+
+@dp.message_handler(state=StudentGroupState.processing)
+async def handle_message_while_processing(message: types.Message, state: FSMContext) -> None:
+    await message.reply(text='Выберите одну из предложенных опций',
+                        reply_markup=get_start_kb())
 
 
 if __name__ == '__main__':
-    Parser.pdf_to_json("example.pdf", "data.json")
-
-    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=on_startup)
+    executor.start_polling(dispatcher=dp, 
+                           skip_updates=True, 
+                           on_startup=on_startup)

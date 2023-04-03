7502db5b72dc1e8e868f85affd65ce8028defa73
SerGoMachine <sereganovik2002@gmail.com>
2023-04-03 22:29:03 +0300

    Пользовательская клавиатура
    Удалил импорт ненужной библиотеки

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,4 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.callback_data import CallbackData
 
 
 def get_start_kb() -> ReplyKeyboardMarkup:

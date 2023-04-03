0855fd81f0f6a74bc212d70327317eb0a5ba35f6
SerGoMachine <sereganovik2002@gmail.com>
2023-04-03 22:34:00 +0300

    Телеграм-бот
    Добавлена совместимость с базой данных.
    Реализован Middleware для корректного восстановления состояния пользователя
    при перезапуске бота.
    Из пофикшенного: добавлена обработка команды /week_schedule (ранее не обрабатывалась)

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from keyboards import *
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
+from database import db_connect, add_user, edit_user_group, edit_user_state, get_user_state
+from aiogram.dispatcher.middlewares import BaseMiddleware
 
 
 bot = Bot(TOKEN)
@@ -12,16 +14,46 @@ dp = Dispatcher(bot, storage=storage)
 
 
 class StudentGroupState(StatesGroup):
+
     processing = State()
     group_number = State()
-    option_proccesing = State()
+    option_processing = State()
     today_schedule = State()
     week_schedule = State()
     dump = State()
 
+    STATES = {
+              'processing': processing, 
+              'group_number': group_number,
+              'option_processing': option_processing,
+              'today_schedule': today_schedule,
+              'week_schedule': week_schedule,
+              'dump': dump
+             }
+
+
+class StateMiddleware(BaseMiddleware):
+
+    async def on_pre_process_message(self, message: types.Message, data: dict) -> None:
+        username = message.from_user.username
+        setattr(StudentGroupState, 'username', username)
+        user_state = await get_user_state(username)
+        cur_state = StudentGroupState.STATES[user_state]
+        await cur_state.set()
+
 
 async def on_startup(_) -> None:
+    await db_connect()
     print('Bot has been started')
+
+
+async def set_state(state: FSMContext, new_state: str) -> None:
+    async with state.proxy() as data:
+        data['state'] = new_state
+        username = StudentGroupState.username
+        
+    await edit_user_state(state, username)
+    await StudentGroupState.STATES[new_state].set()
 
 
 async def cmd_schedule(message: types.Message, reply_markup: ReplyKeyboardMarkup) -> None:
@@ -31,12 +63,14 @@ async def cmd_schedule(message: types.Message, reply_markup: ReplyKeyboardMarkup
 
 
 @dp.message_handler(commands=['start'], state='*')
-async def cmd_start(message: types.Message) -> None:
+async def cmd_start(message: types.Message, state: FSMContext) -> None:
     await bot.send_message(chat_id=message.from_user.id,
                            text='Добро пожаловать!\n'
                                 'Я бот, отправляющий расписание ВМК',
                            reply_markup=get_start_kb())
-    await StudentGroupState.processing.set()
+
+    await add_user(username=message.from_user.username)
+    await set_state(state, 'processing')
 
 
 @dp.message_handler(commands=['help'], state='*')
@@ -47,48 +81,54 @@ async def cmd_help(message: types.Message) -> None:
                                 '/schedule - получить расписание')
 
 
-@dp.message_handler(commands=['schedule'], state=StudentGroupState.processing)
-async def handle_schedule_while_processing(message: types.Message) -> None:
+@dp.message_handler(commands=['schedule'], state=[StudentGroupState.group_number, 
+                                                  StudentGroupState.option_processing, 
+                                                  StudentGroupState.processing])
+async def handle_schedule_while_number(message: types.Message, state: FSMContext) -> None:
     await cmd_schedule(message, reply_markup=get_start_kb())
-    await StudentGroupState.group_number.set()
+    await set_state(state, 'group_number')
 
 
-@dp.message_handler(commands=['schedule'], state=[StudentGroupState.group_number, StudentGroupState.option_proccesing])
-async def handle_schedule_while_number(message: types.Message) -> None:
-    await cmd_schedule(message, reply_markup=get_start_kb())
-    await StudentGroupState.group_number.set()
-
-
-@dp.message_handler(commands=['today_schedule'], state=StudentGroupState.option_proccesing)
-async def cmd_today_schedule(message: types.Message) -> None:
+@dp.message_handler(commands=['today_schedule'], state=StudentGroupState.option_processing)
+async def cmd_today_schedule(message: types.Message, state: FSMContext) -> None:
     await bot.send_message(chat_id=message.from_user.id,
                            text='Держите ваше расписание на сегодня',
                            reply_markup=get_cancel_kb())
-    await StudentGroupState.dump.set()
+    await set_state(state, 'dump')
+
+
+@dp.message_handler(commands=['week_schedule'], state=StudentGroupState.option_processing)
+async def cmd_week_schedule(message: types.Message, state: FSMContext) -> None:
+    await bot.send_message(chat_id=message.from_user.id,
+                            text='Держите ваше расписание на неделю',
+                            reply_markup=get_cancel_kb())
+    await set_state(state, 'dump')
 
 
 @dp.message_handler(commands=['return_to_menu'], state=StudentGroupState.dump)
-async def cmd_cancel(message: types.Message) -> None:
+async def cmd_cancel(message: types.Message, state: FSMContext) -> None:
     await bot.send_message(chat_id=message.from_user.id,
                            text='Вы вернулись в главное меню',
                            reply_markup=get_start_kb())
-    await StudentGroupState.processing.set()
+    await set_state(state, 'processing')
 
 
 @dp.message_handler(commands=['change_group_number'], state=StudentGroupState.dump)
 async def cmd_change(message: types.Message, state: FSMContext) -> None:
     await cmd_schedule(message, reply_markup=get_start_kb())
-    await StudentGroupState.group_number.set()
+    await set_state(state, 'group_number')
 
 
 @dp.message_handler(lambda message: message.text.isdigit(), state=StudentGroupState.group_number)
 async def handle_number(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
-        data['number'] = message.text
+        data['group'] = message.text
+
+    await edit_user_group(state, username=message.from_user.username)
     await bot.send_message(chat_id=message.from_user.id,
                            text='Выберите опцию',
                            reply_markup=get_schedule_option_kb())
-    await StudentGroupState.option_proccesing.set()
+    await set_state(state, 'option_processing')
 
 
 @dp.message_handler(lambda message: not message.text.isdigit(), state=StudentGroupState.group_number)
@@ -105,7 +145,7 @@ async def handle_message_while_dump(message: types.Message, state: FSMContext) -
                            reply_markup=get_cancel_kb())
 
 
-@dp.message_handler(state=StudentGroupState.option_proccesing)
+@dp.message_handler(state=StudentGroupState.option_processing)
 async def handle_message_while_opt(message: types.Message, state: FSMContext) -> None:
     await message.reply(text='Выберите одну из предложенных опций',
                         reply_markup=get_schedule_option_kb())
@@ -118,6 +158,8 @@ async def handle_message_while_processing(message: types.Message, state: FSMCont
 
 
 if __name__ == '__main__':
-    executor.start_polling(dispatcher=dp, 
-                           skip_updates=True, 
+    dp.middleware.setup(StateMiddleware())
+
+    executor.start_polling(dispatcher=dp,
+                           skip_updates=True,
                            on_startup=on_startup)

ad81d90a75cb0c9e77a3782ea76008e2a952d72d
SerGoMachine <sereganovik2002@gmail.com>
2023-04-03 22:29:28 +0300

    База данных
    Добавлена база данных, хранящая информацию о пользователях в виде:
    (username, group_number, state)
    Потребность в базе данных возникла в связи с тем, что необходимо
    сохранять текущее состояние работы пользователя с ботом, чтобы
    при перезапуске бота пользователь не замечал этого и продолжал работу
    с того места, где он остановился
    База данных написана с помощью SQLite (встроенная библиотека в python - sqlite3)

---
+++ database.py
@@ -0,0 +1,38 @@
+import sqlite3 as sq
+from aiogram.dispatcher import FSMContext
+
+
+async def db_connect() -> None:
+	global db, cur
+
+	db = sq.connect('users.db')
+	cur = db.cursor()
+
+	cur.execute('CREATE TABLE IF NOT EXISTS students(username TEXT PRIMARY KEY, number_group TEXT, state TEXT)')
+	db.commit()
+
+
+async def add_user(username: str) -> None:
+	user = cur.execute('SELECT username FROM students WHERE username=="{}"'.format(username)).fetchone()
+	if not user:
+		cur.execute('INSERT INTO students VALUES(?, ?, ?)', (username, '', 'processing'))
+		db.commit()
+
+
+async def edit_user_group(state: FSMContext, username: str) -> None:
+	async with state.proxy() as data:
+		cur.execute('UPDATE students SET number_group="{}" WHERE username=="{}"'.format(data['group'], username))
+		db.commit()
+
+
+async def edit_user_state(state: FSMContext, username: str) -> None:
+	async with state.proxy() as data:
+		cur.execute('UPDATE students SET state="{}" WHERE username=="{}"'.format(data['state'], username))
+		db.commit()
+
+
+async def get_user_state(username: str) -> str:
+	state = cur.execute('SELECT state FROM students WHERE username=="{}"'.format(username)).fetchone()
+	if not state:
+		return 'processing'
+	return state[0]

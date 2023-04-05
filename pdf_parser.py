f4120fda92ff510ed5bbdb293f5426902d821cbc
Pavel <s02200645@gse.cs.msu.ru>
2023-04-05 18:06:14 +0300

    Добавлена документация для модуля pdf_parser.
    Немного изменены имена переменных.

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,46 +1,75 @@
+"""
+"""
+
 import camelot
 import json
 
 
 class Lesson:
+    """Lesson class to be inherited
+
+    Args:
+        start_time (str): Class start time
+        end_time (str): Class finish time
+    """
     start_time = None
     end_time = None
-    pass
 
 
 class NormalLesson(Lesson):
-    CODE = "0"
-    disc = None
+    """Normal lesson
 
-    def __init__(self, st_time: str, ed_time: str, text: str) -> None:
-        self.start_time = st_time
-        self.end_time = ed_time
-        self.disc = text
+    Args:
+        description (str): text that describes normal lesson
+    """
+
+    CODE = "0"
+    description = None
+
+    def __init__(self, start_time: str, end_time: str, description: str) -> None:
+        self.start_time = start_time
+        self.end_time = end_time
+        self.description = description
 
     def __str__(self) -> str:
-        return f"0 {self.CODE} {self.start_time} {self.end_time} {self.disc}"
+        return f"0 {self.CODE} {self.start_time} {self.end_time} {self.description}"
 
 
 class DoubleLesson(Lesson):
-    CODE = "1"
-    disc_up = None
-    disc_down = None
+    """Odd/even week lesson
 
-    def __init__(self, st_time: str, ed_time: str, text_up: str, text_down: str) -> None:
-        self.start_time = st_time
-        self.end_time = ed_time
-        self.disc_up = text_up
-        self.disc_down = text_down
+    Args:
+        description_up (str): description for a odd week lesson
+        description_down (str): description for a even week lesson
+    """
+    CODE = "1"
+    description_up = None
+    description_down = None
+
+    def __init__(self, start_time: str, end_time: str, description_up: str, description_down: str) -> None:
+        self.start_time = start_time
+        self.end_time = end_time
+        self.description_up = description_up
+        self.description_down = description_down
 
     def __str__(self) -> str:
-        return f"1 {self.CODE} {self.start_time} {self.end_time} {self.disc_up} {self.disc_down}"
+        return f"1 {self.CODE} {self.start_time} {self.end_time} {self.description_up} {self.description_down}"
 
 
 class Parser:
+    """PDF table parser, which can transform it into the json data"""
     WEEKDAYS = ("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота")
 
     @staticmethod
     def pdf_to_json(filename_pdf: str, filename_json: str) -> None:
+        """Static method that transform given pdf table into json and store it into the file
+
+        Args:
+            filename_pdf (str): name of pdf table file
+            filename_json (str): name of json file to store data in json format
+        Returns:
+            None
+        """
         data = {}
         with open(filename_json, "r") as f:
             try:
@@ -49,7 +78,7 @@ class Parser:
                     raise json.decoder.JSONDecodeError("Not a dictionary", "", 0)
             except json.decoder.JSONDecodeError:
                 data = {}
-        #print(data)
+        # print(data)
 
         tables = camelot.read_pdf(filename_pdf, line_scale=80, copy_text=['v', 'h'], pages='all')
         for page in tables:
@@ -64,7 +93,7 @@ class Parser:
                     case [week_day] if week_day in Parser.WEEKDAYS:
                         if not group_number_filled:
                             data |= {group_number:
-                                         {day: [] for day in Parser.WEEKDAYS} for group_number in df.loc[i, 1:]}
+                                     {day: [] for day in Parser.WEEKDAYS} for group_number in df.loc[i, 1:]}
                             groups = [group_number for group_number in df.loc[i, 1:]]
                             group_number_filled = True
                         current_weekday = week_day
@@ -88,6 +117,13 @@ class Parser:
 
     @staticmethod
     def pretty_print_json(filename_json: str) -> None:
+        """Method for pretty printing json file
+
+        Args:
+            filename_json (str): name of json file
+        Returns:
+            None
+        """
         data = None
 
         with open(filename_json, "r") as f:
@@ -101,6 +137,13 @@ class Parser:
 
     @staticmethod
     def get_lesson_from_json(filename_json: str, group_number: str) -> dict:
+        """
+        Args:
+            filename_json (str): name of json file with schedule data
+            group_number (str): group number
+        Returns:
+            dict: data in json file that corresponds to group_number parameter
+        """
         with open(filename_json, "r") as f:
             data = json.load(f)
             return data[group_number]

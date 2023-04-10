e0f29e88f572dd247894a7e77ff2e49175b53909
SerGoMachine <sereganovik2002@gmail.com>
2023-04-10 13:48:30 +0300

    Пользовательская клавиатура
    Функции выделены в отдельный класс
    Добавлена документация

--- keyboards.py
+++ keyboards.py
@@ -1,25 +1,51 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+'''Keyboards module for Schedule Telegram bot'''
 
 
-def get_start_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton('/help'),
-         KeyboardButton('/schedule')]
-    ], resize_keyboard=True)
-    return kb
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-def get_schedule_option_kb() -> ReplyKeyboardMarkup:
-	kb = ReplyKeyboardMarkup(keyboard=[
-		[KeyboardButton('/today_schedule'),
-		 KeyboardButton('/week_schedule')]
-	], resize_keyboard=True)
-	return kb
+class Keyboards:
+    '''
+    Class of keyboards for user
+    '''
+    @staticmethod
+    def get_start_kb() -> ReplyKeyboardMarkup:
+        '''
+        Returns initial keyboard
 
+        Returns:
+            ReplyKeyboardMarkup
+        '''
+        kb = ReplyKeyboardMarkup(keyboard=[
+            [KeyboardButton('/help'),
+             KeyboardButton('/schedule')]
+        ], resize_keyboard=True)
+        return kb
 
-def get_cancel_kb() -> ReplyKeyboardMarkup:
-	kb = ReplyKeyboardMarkup(keyboard=[
-		[KeyboardButton('/return_to_menu'),
-		 KeyboardButton('/change_group_number')]
-	], resize_keyboard=True)
-	return kb
+    @staticmethod
+    def get_schedule_option_kb() -> ReplyKeyboardMarkup:
+        '''
+        Returns option keyboard for option_processing state
+
+        Returns:
+            ReplyKeyboardMarkup
+        '''
+        kb = ReplyKeyboardMarkup(keyboard=[
+            [KeyboardButton('/today_schedule'),
+             KeyboardButton('/week_schedule')]
+        ], resize_keyboard=True)
+        return kb
+
+    @staticmethod
+    def get_cancel_kb() -> ReplyKeyboardMarkup:
+        '''
+        Returns keyboard for final state
+
+        Returns:
+            ReplyKeyboardMarkup
+        '''
+        kb = ReplyKeyboardMarkup(keyboard=[
+            [KeyboardButton('/return_to_menu'),
+             KeyboardButton('/change_group_number')]
+        ], resize_keyboard=True)
+        return kb

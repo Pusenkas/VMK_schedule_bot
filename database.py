f139ed3d404ee84490fb114a8cdf1cf5ce093575
SerGoMachine <sereganovik2002@gmail.com>
2023-04-11 21:15:44 +0300

    База данных
    Всё расписание теперь находится в базе данных в отдельной таблице,
    а не в json-файле
    Добавлен соответствующий функционал

--- database.py
+++ database.py
@@ -1,88 +1,128 @@
-'''Database module for Schedule Telegram bot'''
-
+"""Database module for Schedule Telegram bot"""
 
 import sqlite3 as sq
 from aiogram.dispatcher import FSMContext
 
 
 class Database:
-    '''
-    Database class
-    '''
-    async def db_connect(self) -> None:
-        '''Method to connect to database'''
-        self.db = sq.connect('users.db')
-        self.cur = self.db.cursor()
+    """
+    Singleton database class
+    """
 
-        self.cur.execute('CREATE TABLE IF NOT EXISTS students(username TEXT PRIMARY KEY, number_group TEXT, state TEXT)')
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def connect(self) -> None:
+        """Method to connect to database"""
+        if not hasattr(self, 'db'):
+            self.db = sq.connect('users.db')
+            self.cur = self.db.cursor()
+
+        self.cur.execute('CREATE TABLE IF NOT EXISTS students(username TEXT PRIMARY KEY, group_number TEXT, state TEXT)')
+        self.db.commit()
+        self.cur.execute('CREATE TABLE IF NOT EXISTS schedule(group_number TEXT, weekday TEXT, parity BOOL, table_entry TEXT)')
         self.db.commit()
 
-    async def add_user(self, username: str) -> None:
-        '''
+    def update_schedule(self, group_number: str, weekday: str, parity: bool, table_entry: str) -> None:
+        """
+        Method to update table schedule with new entry
+
+        Args:
+            group_number (str): user's group
+            weekday (str): day of the week
+            parity (bool): parity of the week
+            table_entry (str): entry (lesson)
+        """
+        self.cur.execute('INSERT INTO schedule VALUES(?, ?, ?, ?)', (group_number, weekday, parity, table_entry))
+        self.db.commit()
+
+    def get_valid_groups(self) -> list[str]:
+        """
+        Method to get valid groups
+
+        Returns:
+            list[str]
+        """
+        groups = self.cur.execute('SELECT DISTINCT group_number FROM schedule').fetchall()
+        return [group[0] for group in groups]
+
+    def get_schedule(self, group_number: str, parity: bool, weekday: str) -> str:
+        """
+        Method to get schedule by group_number, parity of week and weekday
+
+        Args:
+            group_number (str): user's group
+            parity (bool): parity of the week
+            weekday (str): day of the week
+        Returns:
+            str
+        """
+        schedule = self.cur.execute('SELECT table_entry FROM schedule WHERE group_number=="{}" AND parity=={} AND weekday=="{}"'
+                                    .format(group_number, parity, weekday)).fetchone()
+        return schedule[0]
+
+    def add_user(self, username: str) -> None:
+        """
         Method to add a new user
 
         Args:
-            username (str): username 
-        Returns:
-            None
-        '''
+            username (str): username
+        """
         user = self.cur.execute('SELECT username FROM students WHERE username=="{}"'.format(username)).fetchone()
         if not user:
             self.cur.execute('INSERT INTO students VALUES(?, ?, ?)', (username, '', 'processing'))
             self.db.commit()
 
     async def edit_user_group(self, state: FSMContext, username: str) -> None:
-        '''
+        """
         Method to edit user's group
 
         Args:
             state (FSMContext): user's state
             username (str): username
-        Returns:
-            None
-        '''
+        """
         async with state.proxy() as data:
-            self.cur.execute('UPDATE students SET number_group="{}" WHERE username=="{}"'.format(data['group'], username))
+            self.cur.execute('UPDATE students SET group_number="{}" WHERE username=="{}"'.format(data['group'], username))
             self.db.commit()
 
     async def edit_user_state(self, state: FSMContext, username: str) -> None:
-        '''
+        """
         Method to edit user's state
 
         Args:
             state (FSMContext): user's state
             username (str): username
-        Returns:
-            None
-        '''
+        """
         async with state.proxy() as data:
             self.cur.execute('UPDATE students SET state="{}" WHERE username=="{}"'.format(data['state'], username))
             self.db.commit()
 
-    async def get_user_state(self, username: str) -> str:
-        '''
+    def get_user_state(self, username: str) -> str:
+        """
         Method to get user's state
 
         Args:
             username (str): username
         Returns:
             str
-        '''
+        """
         state = self.cur.execute('SELECT state FROM students WHERE username=="{}"'.format(username)).fetchone()
         if not state:
             return 'processing'
         return state[0]
 
-    async def get_user_group(self, username: str) -> str:
-        '''
+    def get_user_group(self, username: str) -> str:
+        """
         Method to get user's group
 
         Args:
             username (str): username
         Returns:
             str
-        '''
-        state = self.cur.execute('SELECT number_group FROM students WHERE username=="{}"'.format(username)).fetchone()
+        """
+        state = self.cur.execute('SELECT group_number FROM students WHERE username=="{}"'.format(username)).fetchone()
         if not state:
             return 'none'
         return state[0]

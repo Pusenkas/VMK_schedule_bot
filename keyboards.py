cd21f60f2d99b6d2445aae885f617089be4a7ec5
SerGoMachine <sereganovik2002@gmail.com>
2023-04-11 21:16:52 +0300

    Пользовательская клавиатура
    Небольшие поправки по документации

--- keyboards.py
+++ keyboards.py
@@ -1,21 +1,21 @@
-'''Keyboards module for Schedule Telegram bot'''
-
+"""Keyboards module for Schedule Telegram bot"""
 
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 class Keyboards:
-    '''
+    """
     Class of keyboards for user
-    '''
+    """
+    
     @staticmethod
     def get_start_kb() -> ReplyKeyboardMarkup:
-        '''
+        """
         Returns initial keyboard
 
         Returns:
             ReplyKeyboardMarkup
-        '''
+        """
         kb = ReplyKeyboardMarkup(keyboard=[
             [KeyboardButton('/help')]
         ], resize_keyboard=True)
@@ -23,15 +23,16 @@ class Keyboards:
 
     @staticmethod
     def get_cancel_kb() -> ReplyKeyboardMarkup:
-        '''
+        """
         Returns keyboard for final state
 
         Returns:
             ReplyKeyboardMarkup
-        '''
+        """
         kb = ReplyKeyboardMarkup(keyboard=[
-            [KeyboardButton('/return_to_menu')],
-            [KeyboardButton('/today_schedule')],
-            [KeyboardButton('/week_schedule')]
+            [KeyboardButton('Расписание на сегодня  ▶️')],
+            [KeyboardButton('Расписание на завтра  ⏩')],
+            [KeyboardButton('Расписание на неделю  ⏭')],
+            [KeyboardButton('Вернуться назад  ↩️')]
         ], resize_keyboard=True)
         return kb
